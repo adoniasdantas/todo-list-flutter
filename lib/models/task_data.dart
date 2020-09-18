301a3c38e8d8de9549c1aa529da7be4d83079da5
adoniasdantas <adoniasdantas@gmail.com>
2020-09-18 19:33:55 -0300

    finished project

---
+++ lib/models/task_data.dart
@@ -0,0 +1,35 @@
+import 'dart:collection';
+import 'package:flutter/foundation.dart';
+import 'package:todoey_flutter/models/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskName) {
+    final task = Task(name: newTaskName);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void toggleIsDone(int index) {
+    _tasks[index].toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(int index) {
+    _tasks.removeAt(index);
+    notifyListeners();
+  }
+}
